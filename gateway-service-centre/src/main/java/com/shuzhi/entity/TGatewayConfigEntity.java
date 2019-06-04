fb3c2f6a731fb5f32e4b13db6bcc8afafe22e76c
jqgodlike <18647055335@163.com>
2019-06-04 13:45:38 +0800

    jpa 整合postgretsql

---
+++ gateway-service-centre/src/main/java/com/shuzhi/entity/TGatewayConfigEntity.java
@@ -0,0 +1,28 @@
+package com.shuzhi.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+/**
+* @Program:  网关服务配置表
+* @Description:
+* @Author: YuJQ
+* @Create: 2019/6/4 13:15
+**/
+@Data
+@Entity
+@Table(name = "t_gateway_config")
+public class TGatewayConfigEntity {
+    @Id
+    private int id;
+    @Column(name = "sys_id")
+    private String sysId;
+    @Column(name = "connect_id")
+    private String connectId;
+    @Column(name = "type_group_code")
+    private String typeGroupCode;
+    @Column(name = "describe")
+    private String describe;
+}
