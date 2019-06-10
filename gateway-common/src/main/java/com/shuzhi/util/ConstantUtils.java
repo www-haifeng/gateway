cc63633a3a9f1bb115cbe2bbe802d12abf482d6e
jqgodlike <18647055335@163.com>
2019-06-10 17:22:34 +0800

    静态常量

---
+++ gateway-common/src/main/java/com/shuzhi/util/ConstantUtils.java
@@ -0,0 +1,28 @@
+package com.shuzhi.util;
+/**
+* @Program: ConstantUtils
+* @Description:
+* @Author: YuJQ
+* @Create: 2019/6/10 14:05
+**/
+public class ConstantUtils {
+
+    public static final String   KYE_MESSAGE="message";
+    public static final String   KYE_MSG="msg";
+    public static final String   KYE_TIME_STAMP="timestamp";
+
+
+
+
+    public static final Integer KYE_0=0;//0:心跳
+    public static final Integer KYE_1=1;//1：命令
+    public static final Integer KYE_2=2;// 2：回执
+    public static final Integer KYE_3=3;//3：命令执行结果
+    public static final Integer KYE_4=4;//4：上报信息
+    public static final Integer KYE_5=5;//5：首次建立连接
+    public static final Integer KYE_6=6;//6、建连回复
+    public static final Integer KYE_7=7;// 7、告警信息
+
+
+
+}
