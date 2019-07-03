8cdb26d11daa62ef4e3f54857dc231b8d9ac3947
jqgodlike <18647055335@163.com>
2019-07-03 11:42:42 +0800

    网关日志详情

--- gateway-service-centre/src/main/java/com/shuzhi/controller/TestDemo.java
+++ gateway-service-centre/src/main/java/com/shuzhi/controller/TestDemo.java
@@ -4,6 +4,7 @@ package com.shuzhi.controller;
 import com.shuzhi.dao.TGatewayConfigEntityRepository;
 import com.shuzhi.dao.TLogEntityRepository;
 import com.shuzhi.entity.TGatewayConfigEntity;
+import com.shuzhi.entity.TLogEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +30,12 @@ public class TestDemo {
             System.out.println(t);
             logger.info("这个有问题"+t);
         }
+        return "hello 服务已经启动";
+    }
+    @RequestMapping("/save")
+    public String save() {
+        TLogEntity sysLog = new TLogEntity();
+        tLogEntityRepository.save(sysLog);
         return "hello 服务已经启动";
     }
 
