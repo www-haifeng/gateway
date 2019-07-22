590d93205e9d5167f6ca4b2e8e01c9a0fae0878b
lirb <git@lirb>
2019-07-22 19:26:29 +0800

    添加深醒模块2

---
+++ gateway-videodiscern-sensing/src/main/java/com/shuzhi/GatewayVideodiscernSensingApplication.java
@@ -0,0 +1,13 @@
+package com.shuzhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayVideodiscernSensingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayVideodiscernSensingApplication.class, args);
+	}
+
+}
