263f0359e187d15ef71fca1ea145b81f1abb69f9
lirb <git@lirb>
2019-08-08 14:25:13 +0800

    提交杨雁路照明新建模块

---
+++ gateway-light-iotcomm/src/main/java/com/shuzhi/GatewayLightIotcommApplication.java
@@ -0,0 +1,13 @@
+package com.shuzhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayLightIotcommApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayLightIotcommApplication.class, args);
+	}
+
+}
