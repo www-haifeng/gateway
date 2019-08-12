25cb17f7d09f71acd4c52ca8aab27db57a2f3246
yu.haifeng@shuzhi.ai <yuhaifeng6881>
2019-08-12 17:43:59 +0800

    修改深醒动态人脸识别kafka的mq主题

--- gateway-videodiscern-sensing/src/main/java/com/shuzhi/service/ReportService.java
+++ gateway-videodiscern-sensing/src/main/java/com/shuzhi/service/ReportService.java
@@ -52,7 +52,7 @@ public class ReportService {
             systemInfoData.setSign(utils.getSignVerify(systemInfoData));
             String commandRevertJSON = systemInfoData.toString();
             try {
-                rabbitSender.send("lowerControlMessage",commandRevertJSON);
+                rabbitSender.send("upMessage","upMessage",commandRevertJSON);
                 logger.info("命令回执发送完毕:"+commandRevertJSON);
             } catch (Exception e) {
                 e.printStackTrace();
@@ -66,7 +66,7 @@ public class ReportService {
             systemInfoData.setSign(utils.getSignVerify(systemInfoData));
             String commandRevertJSON = systemInfoData.toString();
             try {
-                rabbitSender.send("lowerControlMessage",commandRevertJSON);
+                rabbitSender.send("upMessage","upMessage",commandRevertJSON);
                 logger.error("命令执行失败，请查看原因:"+commandRevertJSON);
             } catch (Exception e) {
                 e.printStackTrace();
